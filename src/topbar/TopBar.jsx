22be1af1c847dd71f4c3a68876c4dd9677f3ee71
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-03 21:58:11 +0530

    topbar

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -2,8 +2,11 @@ import "./topbar.css"
 export default function TopBar() {
   return (
     <div className='top'>
-<div className="topLeft"><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-square-twitter"></i>
-<i class="fa-brands fa-square-pinterest"></i><i class="fa-brands fa-square-instagram"></i></div>
+<div className="topLeft">
+    <i className="topIcon fa-brands fa-square-facebook"></i>
+    <i className="topIcon fa-brands fa-square-twitter"></i>
+    <i className="topIcon fa-brands fa-square-pinterest"></i>
+    <i className="topIcon fa-brands fa-square-instagram"></i></div>
 <div className="topCenter">
     <ul className="topList">
         <li className="topListItem">HOME</li>
@@ -13,7 +16,14 @@ export default function TopBar() {
 <li className="topListItem">LOGOUT</li>
     </ul>
 </div>
-<div className="topRight"></div>
+<div className="topRight">
+<img
+              className="topImg"
+              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+</div>
     </div>
   )
 }
