6ce44da261231712da1d2cba22ddeb61463b0de8
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-03 21:46:41 +0530

    topbar

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -2,7 +2,10 @@ import "./topbar.css"
 export default function TopBar() {
   return (
     <div className='top'>
-topbar
+<div className="topLeft"><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-square-twitter"></i>
+<i class="fa-brands fa-square-pinterest"></i><i class="fa-brands fa-square-instagram"></i></div>
+<div className="topCenter"></div>
+<div className="topRight"></div>
     </div>
   )
 }
