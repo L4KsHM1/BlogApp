97d5565b08ac82c1b85fb411bcad5b8b2f1103cc
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-06 12:26:16 +0530

    post page

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -3,11 +3,20 @@ import './post.css'
 export default function Post() {
     return (
         <div className="post">
-<img 
-className='postImg'
-src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
- alt="noimage" 
- />
+            <img 
+            className='postImg'
+            src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt="noimage" 
+            />
+            <div className="postInfo">
+                <div className="postCats">
+                    <span className="postCat">Music</span>
+                    <span className="postCat">Life</span>
+                </div>
+                <span className="postTitle">Lorem ipsum dolor, sit amet consectetur
+                     adipisicing elit. Assumenda,Lorem ipsum dolor sit amet.
+                     voluptatibus?</span>
+            </div>
         </div>
     )
 }
