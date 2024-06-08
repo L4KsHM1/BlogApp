b9e2835e2345a7d2f72fe6a62f48d526603a22f3
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-08 18:49:17 +0530

    login css

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import './login.css'
 export default function Login() {
   return (
   <div className='login'>
+    <span className="loginTitle">Login</span>
     <form  className="loginForm">
         <label htmlFor="">Email</label>
         <input type="email" placeholder='Enter your email..' name="" id="" />
