d5d6ce95cfa69f59d84b0e827f49e300b86e179d
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-08 12:57:51 +0530

    form

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -4,9 +4,20 @@ export default function Write() {
   <div className='write'>
     <form action="" className="writeForm">
       <div className="writeFormGroup">
-        <input type="file" name="" id="fileInput" />
-        <input type="text" placeholder="Title" className="writeInput" />
+        <label htmlFor="fileInput">
+          <i class="fa-solid fa-plus"></i>
+          </label>
+        <input type="file" name="" id="fileInput" style={{display:'none'}} />
+        <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
       </div>
+      <div className="writeFormGroup">
+        <textarea
+          placeholder="Tell your story..."
+          type="text"
+          className="writeInput writeText"
+        ></textarea>
+      </div>
+      <button className="writeSubmit">Publish</button>
     </form>
     </div>
 );
