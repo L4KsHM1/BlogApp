8a5e87d81f384de6d66e1862273d308a0d9b4495
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-08 23:03:30 +0530

    router

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 function App() {
   return (
-    <>
+    <Router>
       <TopBar />
-      <Login />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Home /> */}
+        {/* </Route> */}
+      </Routes>
+    </Router>
   );
 }
 
