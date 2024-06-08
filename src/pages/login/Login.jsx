223c47f1041776de18acba8ae2277867945219e8
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-08 18:39:31 +0530

    login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,7 +3,14 @@ import './login.css'
 export default function Login() {
   return (
   <div className='login'>
-    Login
+    <form  className="loginForm">
+        <label htmlFor="">Email</label>
+        <input type="email" placeholder='Enter your email..' name="" id="" />
+        <label htmlFor="">Password</label>
+        <input type="password" placeholder='Enter your password..' name="" id="" />
+        <button className="loginButton">Login </button>
+    </form>
+    <button className="loginRegisterButton">Register</button>
     </div>
 );
 }
