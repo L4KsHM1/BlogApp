f366f7ba1fb675e9229bc76287fd5fd52dc6a013
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-08 18:34:11 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Login />
     </>
   );
 }
