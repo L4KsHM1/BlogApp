c3edb92268b410c28449fc70da04c864ae980b6c
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-08 15:37:24 +0530

    settings page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
+import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 function App() {
   return (
     <>
       <TopBar />
-      {/* <Single /> */}
-      <Write />
+      <Settings />
     </>
   );
 }
