60e35770a5eff69b7c2ed3c0d6029f31cef93789
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-06-26 14:13:18 +0530

    category

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -10,4 +10,12 @@ router.post("/", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+router.get("/", async (req, res) => {
+  try {
+    const cats = await Category.find();
+    res.status(200).json(cats);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 module.exports = router;
